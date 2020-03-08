0ada2061450ad8c79872fdcef6bf8c295fe4e71d
Orklah <ronan_morin@hotmail.com>
2020-03-08 19:38:09 +0100

    load class before registering in Psalm

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,7 @@
 <?php
 namespace Orklah\PsalmStrictVisibility;
 
+use Orklah\PsalmStrictVisibility\Hooks\StrictVisibility;
 use SimpleXMLElement;
 use Psalm\Plugin\PluginEntryPointInterface;
 use Psalm\Plugin\RegistrationInterface;
@@ -10,6 +11,8 @@ class Plugin implements PluginEntryPointInterface
     /** @return void */
     public function __invoke(RegistrationInterface $psalm, ?SimpleXMLElement $config = null)
     {
-        $psalm->registerHooksFromClass(StrictVisibility::class);
+        if(class_exists(StrictVisibility::class)){
+            $psalm->registerHooksFromClass(StrictVisibility::class);
+        }
     }
 }
